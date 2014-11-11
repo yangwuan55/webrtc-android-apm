b7831e07cb2da4fcb0de53efc2448e36e6a52139
Dan Albert <danalbert@google.com>
2014-11-11 10:58:41 -0800

    Move the platform libraries to libc++.
    Bug: 15193147
    Change-Id: I259465ecb5bed5005c717a7e660dc6b28be52c55

--- src/modules/audio_coding/codecs/isac/fix/source/Android.mk
+++ src/modules/audio_coding/codecs/isac/fix/source/Android.mk
@@ -75,10 +75,7 @@ LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../../../../../.. \
     $(LOCAL_PATH)/../../../../../../common_audio/signal_processing/include
 
-ifndef WEBRTC_STL
-LOCAL_SHARED_LIBRARIES += libstlport
-include external/stlport/libstlport.mk
-else
+ifdef WEBRTC_STL
 LOCAL_NDK_STL_VARIANT := $(WEBRTC_STL)
 LOCAL_SDK_VERSION := 14
 LOCAL_MODULE := $(LOCAL_MODULE)_$(WEBRTC_STL)
@@ -119,10 +116,7 @@ LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../../../../../.. \
     $(LOCAL_PATH)/../../../../../../common_audio/signal_processing/include
 
-ifndef WEBRTC_STL
-LOCAL_SHARED_LIBRARIES += libstlport
-include external/stlport/libstlport.mk
-else
+ifdef WEBRTC_STL
 LOCAL_NDK_STL_VARIANT := $(WEBRTC_STL)
 LOCAL_SDK_VERSION := 14
 LOCAL_MODULE := $(LOCAL_MODULE)_$(WEBRTC_STL)
--- src/modules/audio_processing/utility/Android.mk
+++ src/modules/audio_processing/utility/Android.mk
@@ -43,10 +43,7 @@ LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libdl
 
-ifndef WEBRTC_STL
-LOCAL_SHARED_LIBRARIES += libstlport
-include external/stlport/libstlport.mk
-else
+ifdef WEBRTC_STL
 LOCAL_NDK_STL_VARIANT := $(WEBRTC_STL)
 LOCAL_SDK_VERSION := 14
 LOCAL_MODULE := $(LOCAL_MODULE)_$(WEBRTC_STL)
